c3ca8e1a17eef0577f91ee68f6d332bdf2d9293a
Zularshi26 <zularshi.ansari26@gmail.com>
2024-12-05 12:37:37 +0530

    Initial commit with .gitignore

---
+++ frontend/src/categories/Pages/Breakdance.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Breakdance = () => {
+  return (
+    <div>Breakdance</div>
+  )
+}
+
+export default Breakdance
---
+++ frontend/src/categories/Pages/FolkDance.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FolkDance = () => {
+  return (
+    <div>FolkDance</div>
+  )
+}
+
+export default FolkDance
